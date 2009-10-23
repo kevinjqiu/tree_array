079bd88ad2f2c4b89176a02d07a5d3e0ca58ea12
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2009-10-23 14:26:45 -0400

    added unit test skeleton; finished put; need refactoring

---
+++ src/test_tree_array.erl
@@ -0,0 +1,21 @@
+%% Author: kevin.jingqiu
+%% Created: Oct 23, 2009
+%% Description: TODO: Add description to test_tree_array
+-module(test_tree_array).
+
+-include("tree_array.hrl").
+
+-include_lib("eunit/include/eunit.hrl").
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+%% -export([]).
+
+empty_tree_array_test() ->
+    ?assertEqual(#node, tree_array:new()).
+
